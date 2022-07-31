dde7b745ce03b85b860350631548da774040ab29
Anuradha <109025058+anuradha0910@users.noreply.github.com>
2022-07-31 17:59:12 +0530

    Add files via upload

---
+++ randmath.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2><u>JAVASCRIPT MATH</u></h2>
+
+<p>it generates numbers from 1 to 10</p>
+
+<p id="demo"></p>
+
+<script>
+rand=Math.floor(Math.random() * 10)+1;
+ document.write(rand);
+input =parseInt(prompt("enter number"));
+if(input==rand)
+{
+  document.write("matched");
+}
+else{
+ document.write("not matched");
+ }
+</script>
+
+</body>
+</html>
+
+
+
+
+
+
